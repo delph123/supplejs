d2f799e0f1b7177a3306cefc89ef4ad1cf35b1f5
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2022-09-03 14:41:20 +0200

    Add render to render component in an html element

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
+import { header } from "./components";
+import { render } from "./rwr";
+
 import "./style.css";
+
+render(header, document.getElementById("app")!);
--- src/rwr.ts
+++ src/rwr.ts
@@ -1,1 +1,14 @@
+export type RWRElement = string | null;
+
 export const version = "0.1";
+
+export function render(
+  component: () => RWRElement,
+  element: HTMLElement
+): void {
+  let rwrelement = component();
+  if (rwrelement) {
+    const child = document.createTextNode(rwrelement);
+    element.appendChild(child);
+  }
+}
