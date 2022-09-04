1ec9480e0b7d788192386464791efbb2614dfa3a
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2022-09-04 20:55:09 +0200

    Allow passing props to components

--- src/components.ts
+++ src/components.ts
@@ -1,61 +1,76 @@
-import { h, useEffect, useState, version } from "./rwr";
+import { createRenderEffect, h, useEffect, useState, version } from "./rwr";
 
 function Header() {
-  return h("div", undefined, [
-    h("h2", undefined, ["Hello!"]),
-    h("h5", undefined, ["It is ", Clock]),
-  ]);
+  return createRenderEffect(() => {
+    return h("div", undefined, [
+      h("h2", undefined, ["Hello!"]),
+      h("h5", undefined, ["It is ", Clock()]),
+    ]);
+  });
 }
 
 function Footer() {
-  return h("p", { class: "read-the-docs" }, [
-    `This page was created with React-Without-React v${version}`,
-  ]);
+  return createRenderEffect(() => {
+    return h("p", { class: "read-the-docs" }, [
+      `This page was created with React-Without-React v${version}`,
+    ]);
+  });
 }
 
 function Clock() {
   const [counter, setCounter] = useState(10);
+
   useEffect(() => {
     let a = setTimeout(() => setCounter(counter + 1), 1000);
     return () => {
       clearTimeout(a);
     };
   }, [counter]);
-  return new Date().toLocaleTimeString();
+
+  return createRenderEffect(() => {
+    return new Date().toLocaleTimeString();
+  });
 }
 
-function Counter() {
+function Counter(props: { index: number; total: number }) {
   const [counter, setCounter] = useState(10);
 
-  return h("div", { class: "card" }, [
-    `Counter has value ${counter}.`,
-    h(
-      "button",
-      {
-        onclick: () => setCounter(counter + 1),
-      },
-      ["+"]
-    ),
-    h(
-      "button",
-      {
-        onclick: () => setCounter(counter - 1),
-      },
-      ["-"]
-    ),
-  ]);
+  return createRenderEffect(() => {
+    return h("div", { class: "card" }, [
+      `Counter ${props.index + 1} / ${props.total} - with value ${counter}.`,
+      h(
+        "button",
+        {
+          onclick: () => setCounter(counter + 1),
+        },
+        ["+"]
+      ),
+      h(
+        "button",
+        {
+          onclick: () => setCounter(counter - 1),
+        },
+        ["-"]
+      ),
+    ]);
+  });
 }
 
 export function App() {
   const [nbCounter, setNbCounter] = useState(0);
-  const counters = new Array(nbCounter).fill(Counter);
-  let btns = h("div", undefined, [
-    h("button", { onclick: () => setNbCounter(nbCounter + 1) }, [
-      "Add Counter",
-    ]),
-    h("button", { onclick: () => setNbCounter(nbCounter - 1) }, [
-      "Remove Counter",
-    ]),
-  ]);
-  return [Header, btns, ...counters, Footer];
+
+  return createRenderEffect(() => {
+    const counters = new Array(nbCounter)
+      .fill(0)
+      .map((_, i) => Counter({ index: i, total: nbCounter }));
+    let btns = h("div", undefined, [
+      h("button", { onclick: () => setNbCounter(nbCounter + 1) }, [
+        "Add Counter",
+      ]),
+      h("button", { onclick: () => setNbCounter(nbCounter - 1) }, [
+        "Remove Counter",
+      ]),
+    ]);
+    return [Header(), btns, ...counters, Footer()];
+  });
 }
--- src/rwr.ts
+++ src/rwr.ts
@@ -4,7 +4,7 @@ export interface RWRElement {
   childNodes: RWRNode;
 }
 
-type RWRNode = (() => RWRNode) | RWRNode[] | RWRElement | string | null;
+type RWRNode = RWRNode[] | RWRElement | string | null;
 
 export const version = "0.1";
 
@@ -77,27 +77,41 @@ export function useEffect(
   }
 }
 
-export function render(
-  component: RWRNode,
-  element: HTMLElement,
-  top = true
-): void {
-  if (top) {
-    numberOfStates = 0;
-    rerender = () => render(component, element);
-    while (element.firstChild) {
-      element.firstChild.remove();
-    }
-  }
-  if (component == null) {
+export function createRenderEffect(renderEffect: () => RWRNode) {
+  return renderEffect();
+}
+
+function addToDom(container: HTMLElement, domComponent: DOMComponent) {
+  if (domComponent == null) {
     return;
-  } else if (typeof component === "function") {
-    render(component(), element, false);
+  } else if (Array.isArray(domComponent)) {
+    domComponent.forEach(addToDom.bind(null, container));
+  } else {
+    container.appendChild(domComponent);
+  }
+}
+
+export function render(component: () => RWRNode, container: HTMLElement) {
+  numberOfStates = 0;
+  rerender = () => render(component, container);
+  while (container.firstChild) {
+    container.firstChild.remove();
+  }
+
+  addToDom(container, createDOMComponent(component()));
+}
+
+type DOMComponent = Node | null | DOMComponent[];
+
+function createDOMComponent(component: RWRNode): DOMComponent {
+  if (component == null) {
+    return null;
+    // } else if (typeof component === "function") {
+    //   renderInDom(component(), element, false);
   } else if (typeof component === "string") {
-    const child = document.createTextNode(component);
-    element.appendChild(child);
+    return document.createTextNode(component);
   } else if (typeof component === "object" && Array.isArray(component)) {
-    component.forEach((rwrchild) => render(rwrchild, element, false));
+    return component.map(createDOMComponent);
   } else {
     const child = document.createElement(component.name);
     Object.entries(component.attributes).forEach(([name, value]) => {
@@ -107,7 +121,7 @@ export function render(
         child.addEventListener(name.substring(2), value);
       }
     });
-    element.appendChild(child);
-    render(component.childNodes, child, false);
+    addToDom(child, createDOMComponent(component.childNodes));
+    return child;
   }
 }
