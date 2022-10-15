8022700825805a9d5f716aacf3469d4efb2c458b
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2022-10-15 17:59:02 +0200

    Add redux selector

--- src/rwr/store.ts
+++ src/rwr/store.ts
@@ -1,3 +1,15 @@
+import { createMemo } from "./reactivity";
+
 export function createReduxStore() {
-  // TODO
+    // TODO
+}
+
+export function createReduxSelector<T>(source: () => T) {
+    return function selector<U>(
+        fn: (src: T, prev: U) => U,
+        value: U,
+        equals?: (a: U, b: U) => boolean
+    ) {
+        return createMemo((prev) => fn(source(), prev), value, { equals });
+    };
 }
