1f8a77c48f50531c4fe0ce43ded00221cdf7f244
Delphin Barraud <33684201+delph123@users.noreply.github.com>
2023-07-08 23:51:11 +0200

    Add support for <Show>

--- src/main.tsx
+++ src/main.tsx
@@ -13,11 +13,12 @@ import { Indexer, Mapper } from "./examples/iterators";
 import { App } from "./examples/js_framework_bench";
 import { ReduxSlice } from "./examples/redux";
 import RainbowApp from "./examples/RainbowApp";
+import { TestWhen } from "./examples/controls";
 
 // import "./style.css";
 
 const exit = render(
     // () => <ReduxSlice nb={5} onexit={() => exit()} />,
-    () => <RainbowApp />,
+    () => <TestWhen />,
     document.getElementById("app")!
 );
--- src/rwr/controls.tsx
+++ src/rwr/controls.tsx
@@ -1,5 +1,41 @@
-export function Show() {
-    // TODO
+import { createMemo } from "./reactivity";
+import { RWRChild, RWRNode, RWRNodeEffect } from "./types";
+
+type WhenCondition<T> = T | undefined | null | false;
+
+export function Show<T>(props: {
+    when: WhenCondition<T> | (() => WhenCondition<T>);
+    keyed?: boolean;
+    fallback?: RWRChild;
+    children?:
+        | RWRNode[]
+        | [((item: () => T) => RWRNode) | ((item: T) => RWRNode)];
+}): RWRNodeEffect {
+    const when = (
+        typeof props.when === "function" ? props.when : () => props.when
+    ) as () => WhenCondition<T>;
+    const display = createMemo(() => {
+        const v = when();
+        return v != null && v !== false;
+    });
+    return () => {
+        if (display()) {
+            if (
+                props.children?.length === 1 &&
+                typeof props.children?.[0] === "function"
+            ) {
+                return props.children[0](props.when as any);
+            } else {
+                return props.children as RWRNode[];
+            }
+        } else {
+            if (typeof props?.fallback === "function") {
+                return props.fallback();
+            } else {
+                return props?.fallback ?? null;
+            }
+        }
+    };
 }
 
 export function Switch() {
